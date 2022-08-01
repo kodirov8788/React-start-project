bbcb258278729f991a5094a75e2b94c29e3b5182
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-08-01 08:15:39 +0500

    first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const myFunc = () => {
+        console.log("ishladi");
+    }
+    return (
+        <div className="app">
+            <Navbar />
+            <Sidebar />
+            <button onClick={myFunc}>Button</button>
+            <h1>salom dunyo</h1>
+        </div>
+    )
 }
 
 export default App;
