cf11283ba7fa12e76e04446d2e5b0190659ece31
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-08-12 09:45:20 +0500

    adsasd

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Main.css"
 import Card from "../Card/Card"
+import axios from "axios"
 
 
 function Main({ products }) {
-    // console.log(products);
+    const [Time, setTime] = useState("")
+
+    // useEffect(() => {
+    //     const myFunc = () => {
+    //         setTimeout(() => {
+    //             setTime("Salom Uzbekistan")
+    //         }, 2000)
+    //     }
+    //     myFunc()
+    // }, [])
+
+    // useEffect(() => {
+    //     const myFunc = () => {
+    //         setTimeout(() => {
+    //             if (Time !== "") {
+    //                 setTime("")
+    //             }
+    //         }, 2000)
+    //     }
+
+    //     myFunc()
+    // }, [Time])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await axios.get("https://jsonplaceholder.typicode.com/users")
+                .then(res => console.log(res.data))
+                .catch(error => console.log(error))
+        }
+        fetchData()
+
+    }, [])
+
+
+
     return (
         <div className='main'>
             {products.map(product => (
                 <Card key={product.id} product={product} />
             ))
             }
-        </div>
+            {/* <button onClick={myFunc}>Button</button> */}
+            {Time !== "" &&
+                < div className="main_effect">
+                    <h1>{Time}</h1>
+                </div>
+            }
+        </div >
     )
 }
 
