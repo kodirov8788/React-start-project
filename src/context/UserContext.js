3922b2ace1d4b1507cfb311dfffe3427993680a0
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-08-15 09:20:36 +0500

    adsdasd

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,15 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 
 
 export const UserContextApi = ({ children }) => {
+    const [dataApi, setdataApi] = useState(null)
 
-    const value = {
-        name: "Axmadjon",
-        lastname: "Kodirov"
+    const ChangeValue = (Name, LastName) => {
+        setdataApi(...dataApi, Name, LastName)
     }
+    const value = {
+        name: dataApi?.Name,
+        lastname: dataApi?.LastName,
+        ChangeValue
+    }
+
+
+
 
     return < UserContext.Provider value={value} > {children}</UserContext.Provider>
 }
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import img3 from "../../images/about.jpg"
 import "./About.css"
 import { UserContext } from "../../context/UserContext"
@@ -6,16 +6,21 @@ import { UserContext } from "../../context/UserContext"
 
 
 function About() {
+    const [InputData, setInputData] = useState(null)
     const { name, lastname } = useContext(UserContext)
     console.log(name, lastname);
+    // const func = (e) => {
+    //     console.log(e.target.value);
+    // }
 
     return (
         <div >
+            <input style={{ height: "50px", width: "200px" }} type="text" placeholder='Name...'
+                onChange={(e) => console.log(e.target.value)} />
 
 
-            <img className='img' src={img3} alt="" />
-
-
+            <input style={{ height: "50px", width: "200px" }} type="text" placeholder='Last Name...'
+                onChange={(e) => console.log(e.target.value)} />
         </div>
     )
 }
