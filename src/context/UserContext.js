fe188030c48474ed69bbc3ec52ecfd77a28fbd66
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-08-19 08:15:51 +0500

    adasdksa

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -2,9 +2,8 @@ import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
-
-
 export const UserContextApi = ({ children }) => {
+
     const [dataApi, setdataApi] = useState({
         Name: "", LastName: ""
     })
@@ -12,14 +11,12 @@ export const UserContextApi = ({ children }) => {
     const ChangeValue = (Name, LastName) => {
         setdataApi({ ...dataApi, Name, LastName })
     }
+
     const value = {
         name: dataApi?.Name,
         lastname: dataApi?.LastName,
         ChangeValue
     }
-
-
-
 
     return < UserContext.Provider value={value} > {children}</UserContext.Provider>
 }
